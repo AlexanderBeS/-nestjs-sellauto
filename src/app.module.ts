0d89954b89b691bb813bab726b880752a39c27f1
Alexander Bespalov <alexander.bespalov@clockwise.software>
2021-11-08 17:34:37 +0200

    Added env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,38 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {UsersModule} from './users/users.module';
 import {ReportsModule} from './reports/reports.module';
+import {ConfigModule, ConfigService} from "@nestjs/config";
 import {User} from "./users/user.entity";
 import {Report} from "./reports/report.entity";
 
 const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true
+        }
+      }
+    })
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true
+    // })
+  ],
   controllers: [AppController],
   providers: [
     AppService,
