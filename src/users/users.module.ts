1816cc5925a54359faa77a809cbec8047dca9218
Alexander Bespalov <alexander.bespalov@clockwise.software>
2021-11-11 13:08:24 +0200

    Added middleware for user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,27 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
-import { User } from "./user.entity";
+import {Module, MiddlewareConsumer} from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsersController} from './users.controller';
+import {UsersService} from './users.service';
+import {User} from "./user.entity";
 import {AuthService} from "./auth.service";
 import {CurrentUserInterceptor} from "./interceptors/current-user.interceptor";
 import {APP_INTERCEPTOR} from "@nestjs/core";
+import {CurrentUserMiddleware} from "./middlewares/current-user.middleware";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], //связываем этот модуль с entity
   controllers: [UsersController],
-  providers: [UsersService, AuthService, {
-    provide: APP_INTERCEPTOR,
-    useClass: CurrentUserInterceptor
-  }]
+  providers: [
+    UsersService,
+    AuthService,
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CurrentUserInterceptor
+    // }
+  ]
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*')
+  }
+}
